92cfab6768e24018d533f775d5885f51c09742ac
Friedonia <schmidt.frieder@icloud.com>
2023-08-03 13:52:32 +0200

    made code easyer to read

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var note = 0;
+const fragen = 15;
+
 document.getElementById('submit').onclick =  function() {
-    for (let i = 1; i < 16; i++) {
+    for (let i = 1; i < fragen+1; i++) {
         
         const antwort = document.getElementById(''+ i);
         antwort.disabled = true;
@@ -8,7 +10,7 @@ document.getElementById('submit').onclick =  function() {
             note += parseInt(antwort.value);
         }
     }
-        note = note/5 * 15;
+        note = note/5 * fragen;
         alert("Deine Note ist: "+ note + "! \nNächstes mal wird es besser sein!");
         document.getElementById('submit').disabled = true;
         console.log(note);
